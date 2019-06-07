a81d173f4898632219d1f37b68e6208f006c4246
Victor Marks <vjmarks@gmail.com>
2019-06-06 23:23:20 -0700

    added math logic

--- src/App.js
+++ src/App.js
@@ -3,14 +3,28 @@ import './App.css';
 import { Button } from './components/Button';
 import { Input } from './components/Input';
 import { ClearButton } from './components/ClearButton';
+import * as math from 'mathjs';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      input: ''
-    };
-  }
+  state = {
+    input: ''
+  };
+
+  createExpression = val => {
+    if (isNaN(val) && isNaN(this.state.input[this.state.input.length - 1])) {
+      this.setState({ input: this.state.input });
+    } else {
+      this.setState({ input: this.state.input + val });
+    }
+  };
+
+  handleEqual = () => {
+    if (isNaN(this.state.input[this.state.input.length - 1])) {
+      this.setState({ input: this.state.input });
+    } else {
+      this.setState({ input: math.eval(this.state.input) });
+    }
+  };
 
   render() {
     return (
@@ -18,28 +32,28 @@ class App extends Component {
         <div className="calc-wrapper">
           <Input input={this.state.input} />
           <div className="row">
-            <Button>7</Button>
-            <Button>8</Button>
-            <Button>9</Button>
-            <Button>/</Button>
+            <Button handleClick={this.createExpression}>7</Button>
+            <Button handleClick={this.createExpression}>8</Button>
+            <Button handleClick={this.createExpression}>9</Button>
+            <Button handleClick={this.createExpression}>/</Button>
           </div>
           <div className="row">
-            <Button>4</Button>
-            <Button>5</Button>
-            <Button>6</Button>
-            <Button>X</Button>
+            <Button handleClick={this.createExpression}>4</Button>
+            <Button handleClick={this.createExpression}>5</Button>
+            <Button handleClick={this.createExpression}>6</Button>
+            <Button handleClick={this.createExpression}>*</Button>
           </div>
           <div className="row">
-            <Button>1</Button>
-            <Button>2</Button>
-            <Button>3</Button>
-            <Button>+</Button>
+            <Button handleClick={this.createExpression}>1</Button>
+            <Button handleClick={this.createExpression}>2</Button>
+            <Button handleClick={this.createExpression}>3</Button>
+            <Button handleClick={this.createExpression}>+</Button>
           </div>
           <div className="row">
-            <Button>.</Button>
-            <Button>0</Button>
-            <Button>=</Button>
-            <Button>-</Button>
+            <Button handleClick={this.createExpression}>.</Button>
+            <Button handleClick={this.createExpression}>0</Button>
+            <Button handleClick={() => this.handleEqual()}>=</Button>
+            <Button handleClick={this.createExpression}>-</Button>
           </div>
           <div className="row">
             <ClearButton handleClear={() => this.setState({ input: '' })}>
