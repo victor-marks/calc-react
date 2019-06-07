8a31a1b620f4f03697b09af3fb24d71eefd9851c
Victor Marks <vjmarks@gmail.com>
2019-06-06 20:32:03 -0700

    added rest of buttons with style

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
-import Button from './components/Button';
+import { Button } from './components/Button';
+import { Input } from './components/Input';
+import { ClearButton } from './components/ClearButton';
 
 class App extends Component {
   constructor(props) {
@@ -14,11 +16,35 @@ class App extends Component {
     return (
       <div className="app">
         <div className="calc-wrapper">
+          <Input input={this.state.input} />
           <div className="row">
             <Button>7</Button>
             <Button>8</Button>
             <Button>9</Button>
             <Button>/</Button>
+          </div>
+          <div className="row">
+            <Button>4</Button>
+            <Button>5</Button>
+            <Button>6</Button>
+            <Button>X</Button>
+          </div>
+          <div className="row">
+            <Button>1</Button>
+            <Button>2</Button>
+            <Button>3</Button>
+            <Button>+</Button>
+          </div>
+          <div className="row">
+            <Button>.</Button>
+            <Button>0</Button>
+            <Button>=</Button>
+            <Button>-</Button>
+          </div>
+          <div className="row">
+            <ClearButton handleClear={() => this.setState({ input: '' })}>
+              Clear
+            </ClearButton>
           </div>
         </div>
       </div>
